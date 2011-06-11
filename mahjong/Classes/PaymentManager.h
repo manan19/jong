2001f6958ffaf37ad35233e2611840c17423a752
Manan <manan19@gmail.com>
2011-06-11 16:32:27 -0700

    new promo art

---
+++ mahjong/Classes/PaymentManager.h
@@ -0,0 +1,16 @@
+//
+//  PaymentManager.h
+//  Mahjongg
+//
+//  Created by Manan Patel on 6/11/11.
+//  Copyright 2011 ngmoco:). All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+
+@interface PaymentManager : NSObject {
+    
+}
+
+@end
